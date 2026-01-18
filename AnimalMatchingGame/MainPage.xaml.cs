2b00c3acd1106107e411464351e17d4d73b5162a
dejmitchem <dejmitchem@icloud.com>
2026-01-18 17:03:01 +0000

    Working game, missing timer

--- AnimalMatchingGame/MainPage.xaml.cs
+++ AnimalMatchingGame/MainPage.xaml.cs
@@ -34,9 +34,39 @@
             }
         }
 
+        Button lastClicked;
+        bool findingMatch = false;
+        int matchesFound = 0;
         private void Button_Clicked(object sender, EventArgs e)
         {
+            if (sender is Button buttonClicked)
+            {
+                if (!string.IsNullOrWhiteSpace(buttonClicked.Text) && (findingMatch == false))
+                {
 
+                    buttonClicked.BackgroundColor = Colors.Red;
+                    lastClicked = buttonClicked;
+                    findingMatch = true;
+                }
+                else
+                {
+                    if ((buttonClicked != lastClicked) && (buttonClicked.Text == lastClicked.Text) && (buttonClicked.Text != " "))
+                    {
+                        matchesFound++;
+                        lastClicked.Text = " ";
+                        buttonClicked.Text = " ";
+                    }
+                    lastClicked.BackgroundColor = Colors.LightBlue;
+                    buttonClicked.BackgroundColor = Colors.LightBlue;
+                    findingMatch = false;
+                }
+            }
+            if (matchesFound == 8)
+            {
+                AnimalButtons.IsVisible = false;
+                PlayAgainButton.IsVisible = true;
+                matchesFound = 0;
+            }
+            }
         }
     }
-}
