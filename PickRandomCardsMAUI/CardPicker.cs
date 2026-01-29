82523996981c213679b0eb20ed64462cd77c306b
dejmitchem <dejmitchem@icloud.com>
2026-01-29 16:22:45 +0000

    Maui card picker

---
+++ PickRandomCardsMAUI/CardPicker.cs
@@ -0,0 +1,35 @@
+﻿namespace PickRandomCards;
+
+
+
+internal class CardPicker
+{
+    public static string[] PickSomeCards(int numberOfCards)
+    {
+
+
+        string[] pickedCards = new string[numberOfCards];
+        for (int i = 0; i < numberOfCards; i++)
+        {
+            pickedCards[i] = RandomValue() + " of " + RandomSuit();
+        }
+        return pickedCards;
+    }
+
+    private static string RandomSuit()
+    {
+        string[] suits = ["Hearts", "Diamonds", "Clubs", "Spades"];
+        int value = Random.Shared.Next(0, 4);
+        return suits[value];
+    }
+
+    private static string RandomValue()
+    {
+        int value = Random.Shared.Next(1, 14);
+        if (value == 1) return "Ace";
+        if (value == 11) return "Jack";
+        if (value == 12) return "Queen";
+        if (value == 13) return "King";
+        return value.ToString();
+    }
+}
---
+++ PickRandomCardsMAUI/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using PickRandomCards;
+
+namespace PickRandomCardsMAUI
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void PickCardsButton_Clicked(object sender, EventArgs e)
+        {
+            if (int.TryParse(NumberOfCards.Text, out int numberOfCardsInt))
+            {
+                string[] cards = CardPicker.PickSomeCards(numberOfCardsInt);
+                foreach (string card in cards)
+                {
+                    PickedCards.Text += card + Environment.NewLine;
+                }
+            }
+        
+            else { 
+                PickedCards.Text = "Please enter a valid number.";
+            }
+            
+            
+
+        }
+    }
+}
