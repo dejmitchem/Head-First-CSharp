a7e6feced45b38ef141d4d619d5681f1496d53ba
dejmitchem <dejmitchem@icloud.com>
2026-02-12 14:01:31 +0000

    Grid Example Maui

---
+++ GridExample/MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace GridExample
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
---
+++ TestingTheRandomClass/Program.cs
@@ -0,0 +1,13 @@
+﻿int zeroToNine = Random.Shared.Next(10);
+Console.WriteLine(zeroToNine);
+
+int dieRoll = Random.Shared.Next(1, 7);
+Console.WriteLine(dieRoll);
+
+double randomDouble = Random.Shared.NextDouble();
+Console.WriteLine((float)randomDouble * 100F);
+Console.WriteLine((decimal)randomDouble * 100M);
+
+int zeroOrOne = Random.Shared.Next(2);
+bool coinFlip= Convert.ToBoolean(zeroOrOne);
+Console.WriteLine(coinFlip);
