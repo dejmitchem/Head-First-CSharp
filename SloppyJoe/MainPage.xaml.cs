d4573071a1971ef6ca0dd43813aeb8cf492efaa5
dejmitchem <dejmitchem@icloud.com>
2026-02-13 18:03:29 +0000

    InitializeComponent not being recognised

---
+++ SloppyJoe/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿
+namespace SloppyJoe;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+        MakeTheMenu();
+    }
+
+    private void MakeTheMenu()
+    {
+        MenuItems[] menuItems = new MenuItems[6];
+
+        for (int i = 0; i < 6; i++)
+        {
+            menuItems[i] = new MenuItems();
+            menuItems[i].Generate();
+        }
+
+        price1.Text = menuItems[0].Price;
+        item1.Text = menuItems[0].Description;
+        price2.Text = menuItems[1].Price;
+        item2.Text = menuItems[1].Description;
+        price3.Text = menuItems[2].Price;
+        item3.Text = menuItems[2].Description;
+        price4.Text = menuItems[3].Price;
+        item4.Text = menuItems[3].Description;
+        price5.Text = menuItems[4].Price;
+        item5.Text = menuItems[4].Description;
+        price6.Text = menuItems[5].Price;
+        item6.Text = menuItems[5].Description;
+
+        MenuItems guacamoleMenuItem = new MenuItems();
+        guacamoleMenuItem.Generate();
+        guacamole.Text = "Add guacamole for " + guacamoleMenuItem.Price;
+    }
+}
---
+++ SloppyJoe/MenuItems.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace SloppyJoe;
+
+class MenuItems
+{
+    public string[] Proteins = [
+                "Roast beef", "Salami", "Turkey",
+                "Ham", "Pastrami", "Tofu"
+    ];
+
+    public string[] Condiments = [
+                "yellow mustard", "brown mustard",
+                "honey mustard", "mayo", "relish", "French dressing"
+    ];
+
+    public string[] Breads = ["rye", "white", "wheat", "pumpernickel", "a roll"];
+
+    public string Description = "";
+    public string Price = "";
+
+    public void Generate()
+    {
+        string protein = Proteins[Random.Shared.Next(Proteins.Length)];
+        string condiment = Condiments[Random.Shared.Next(Condiments.Length)];
+        string bread = Breads[Random.Shared.Next(Breads.Length)];
+        Description = protein + " with " + condiment + " on " + bread;
+
+        int bucks = Random.Shared.Next(5, 15);
+        int cents = Random.Shared.Next(0, 100);
+        decimal price = bucks + (cents * .01M);
+        Price = price.ToString("c");
+    }
+}
+
