f2acc28c7b11af182a56eb1ab409ab48dac4e317
dejmitchem <dejmitchem@icloud.com>
2026-02-19 15:43:19 +0000

    Debugged MAUI Damage Calculator

---
+++ MAUIDamageCalculator/MainPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿namespace MAUIDamageCalculator
+{
+    public partial class MainPage : ContentPage
+    {
+        SwordDamage swordDamage = new SwordDamage();
+
+        public MainPage()
+        {
+            InitializeComponent();
+            swordDamage.SetMagic(Magic.IsChecked);
+            swordDamage.SetFlaming(Flaming.IsChecked);
+            RollDice();
+        }
+
+        private void RollDice()
+        {
+            swordDamage.Roll = Random.Shared.Next(1, 7) + Random.Shared.Next(1, 7)
+                + Random.Shared.Next(1,7);
+            swordDamage.SetMagic(Magic.IsChecked);
+            swordDamage.SetFlaming(Flaming.IsChecked);
+            DisplayDamage();
+        }
+
+        private void DisplayDamage()
+        {
+            Damage.Text = $"Rolled {swordDamage.Roll} for {swordDamage.Damage} HP";
+        }
+
+        private void Flaming_CheckedChanged(object sender, CheckedChangedEventArgs e)
+        {
+            swordDamage.SetFlaming(e.Value);
+            DisplayDamage();
+        }
+
+        private void Magic_CheckedChanged(object sender, CheckedChangedEventArgs e)
+        {
+            swordDamage.SetMagic(e.Value);
+            swordDamage.SetFlaming(Flaming.IsChecked);
+            DisplayDamage();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            RollDice();
+        }
+
+    }
+}
---
+++ MAUIDamageCalculator/SwordDamage.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Diagnostics;
+
+namespace MAUIDamageCalculator
+{
+    internal class SwordDamage
+    {
+        public const int BASE_DAMAGE = 3;
+        public const int FLAME_DAMAGE = 2;
+
+        public int Roll;
+        public decimal MagicMultiplier = 1M;
+        public int Damage;
+
+        public void CalculateDamage()
+        {
+            Damage = (int)(Roll * MagicMultiplier) + BASE_DAMAGE;
+            Debug.WriteLine($"CalculateDamage set Damage to: {Damage} (Roll: {Roll}");
+        }
+
+        public void SetMagic(bool isMagic)
+        {
+            if (isMagic)
+                MagicMultiplier = 1.75M;
+            else
+                MagicMultiplier = 1M;
+            CalculateDamage();
+            Debug.WriteLine($"SetMagic set Damage to: {Damage} (Roll: {Roll}");
+        }
+
+        public void SetFlaming(bool isFlaming)
+        {
+            CalculateDamage();
+            if (isFlaming)
+                Damage += FLAME_DAMAGE;
+            Debug.WriteLine($"SetFlaming set Damage to: {Damage} (Roll: {Roll}");
+        }
+    }
+}
