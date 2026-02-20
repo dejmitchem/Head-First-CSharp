3d402f8bc8f5ca6069064a5f7c5263db6884c1b2
dejmitchem <dejmitchem@icloud.com>
2026-02-20 13:19:52 +0000

    Paintball game, encapsulation practice

---
+++ Paintball/PaintballGun.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Paintball
+{
+    class PaintballGun
+    {
+
+        public PaintballGun(int balls, int magazineSize, bool loaded) 
+        { 
+            this.balls = balls;
+            MagazineSize = magazineSize;
+            if (loaded) Reload();
+        }
+
+
+        public int MagazineSize { get; private set; }
+
+        private int balls;
+        public int BallsLoaded { get; private set; }
+
+        public bool IsEmpty() { return BallsLoaded == 0; }
+
+        public int Balls
+        { 
+            get { return balls; }
+
+            set
+            {
+                if(value > 0) balls = value;
+                Reload();
+            }
+        }
+        public void Reload()
+        {
+            if (balls > MagazineSize)
+                BallsLoaded = MagazineSize;
+            else BallsLoaded = balls;
+        }
+
+        public bool Shoot()
+        {
+            if(BallsLoaded == 0) return false;
+            BallsLoaded--;
+            balls--;
+            return true;
+        }
+
+    }
+}
