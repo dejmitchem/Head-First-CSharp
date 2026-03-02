5c8429983417f3678e8adf7859722d8700b7422b
dejmitchem <dejmitchem@icloud.com>
2026-03-02 15:50:04 +0000

    Bee hive management system XAML written

---
+++ TheBeeHiveManagementSystem/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace TheBeeHiveManagementSystem
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ TheBeeHiveManagementSystem/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿namespace TheBeeHiveManagementSystem
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            JobPicker.ItemsSource = new string[]
+            {
+                "Nectar Collector",
+                "Honey Manufacturer",
+                "Egg Care",
+            };
+
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void WorkShiftButton_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void OutOfHoneyButton_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
