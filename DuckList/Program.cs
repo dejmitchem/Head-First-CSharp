31d5e258e0d6c2549e63c2d01b7de23d87b4c4b5
dejmitchem <dejmitchem@icloud.com>
2026-03-09 16:57:56 +0000

    List sorting

---
+++ DuckList/Program.cs
@@ -0,0 +1,21 @@
+﻿using DuckList;
+
+List<Duck> ducks = [
+    new Duck(17, KindOfDuck.Mallard),
+    new Duck(18, KindOfDuck.Muscovy),
+    new Duck(14, KindOfDuck.Loon),
+    new Duck(11, KindOfDuck.Muscovy),
+    new Duck(14, KindOfDuck.Mallard),
+    new Duck(13, KindOfDuck.Loon)
+    ];
+
+ducks.Sort();
+PrintDucks(ducks);
+
+void PrintDucks(List<Duck> ducks)
+{
+    foreach (var duck in ducks)
+    {
+        Console.WriteLine($"{duck.Size} inch {duck.Kind}");
+    }
+}
