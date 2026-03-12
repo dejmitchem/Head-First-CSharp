d617b02c7f04958715c76476ec3fd4145a633697
dejmitchem <dejmitchem@icloud.com>
2026-03-12 17:43:27 +0000

    Card Shuffler

---
+++ CardShuffler/Card.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CardClass
+{
+    class Card(Suits suit, Values value)
+    {
+        public Suits Suit { get { return suit; } }
+        public Values Value { get { return value; } }
+        public string Name { get { return $"{Value} of {Suit}"; } }
+    }
+}
---
+++ CardShuffler/Program.cs
@@ -0,0 +1,33 @@
+﻿using CardClass;
+
+
+List<Card> cards = new List<Card>();
+
+
+Console.Write("How many cards would you like to draw? ");
+if (int.TryParse(Console.ReadLine(), out int numberOfCards)) 
+{ 
+     for (int i = 0; i < numberOfCards; i++)
+            cards.Add(RandomCard());
+}
+else
+    Console.WriteLine("Invalid input, try again.");
+
+
+CardComparerByValue comparer = new CardComparerByValue();
+cards.Sort(comparer);
+PrintCards(cards);
+
+
+
+
+static Card RandomCard()
+{
+    return new Card((Suits)Random.Shared.Next(0, 4), (Values)Random.Shared.Next(1,14));
+}   
+
+static void PrintCards(List<Card> cards)
+{
+    foreach (Card card in cards)
+        Console.WriteLine(card.Name);
+}
