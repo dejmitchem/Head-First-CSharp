5f60351c598aaa32517e2004ffee65275d94b1b9
dejmitchem <dejmitchem@icloud.com>
2026-03-15 14:19:14 +0000

    Lumberjack Flapjack Stack App

---
+++ LumberjackFlapjackApp/Lumberjack.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace LumberjackFlapjackApp
+{
+    class Lumberjack
+    {
+        private string name;
+        public string Name { get { return name; } }
+
+        private Stack<Flapjack> flapjackStack = new Stack<Flapjack>();
+
+        public Lumberjack(string name)
+        {
+            this.name = name;
+        }
+
+        public void TakeFlapjacks(Flapjack flapjack)
+        {
+            flapjackStack.Push(flapjack);
+        }
+
+        public bool EatFlapjacks()
+        {
+            if (flapjackStack.Count == 0) return false;
+
+            Console.WriteLine($"{Name} ate a {flapjackStack.Pop()} flapjack");
+            return true;
+        }
+    }
+}
---
+++ LumberjackFlapjackApp/Program.cs
@@ -0,0 +1,37 @@
+﻿using LumberjackFlapjackApp;
+
+Queue<Lumberjack> lumberjacks = new Queue<Lumberjack>();
+
+
+while (true)
+{
+    Console.Write("Next lumberjack's name (blank to end:) ");
+    string? name = Console.ReadLine();
+    
+    if (name == "") break;
+
+    Console.Write("Number of flapjacks: ");
+    string? inputFlapjacks = Console.ReadLine();
+
+    if (int.TryParse(inputFlapjacks, out int numFlapjacks))
+    {
+        Lumberjack nextLumberJack = new Lumberjack(name);
+        for (int i = 0; i < numFlapjacks; i++)
+        {
+            Flapjack flapjack = (Flapjack)Random.Shared.Next(0,4);
+            nextLumberJack.TakeFlapjacks(flapjack);
+        }
+        lumberjacks.Enqueue(nextLumberJack);
+    }
+    else Console.WriteLine("Invalid value, try again");
+}
+
+for(int i = 0; i <= lumberjacks.Count(); i++)
+{
+    while (true)
+    {
+        if (!lumberjacks.Peek().EatFlapjacks()) break;
+    }
+    lumberjacks.Dequeue();
+}
+
